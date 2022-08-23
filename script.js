b5f1479de6c9bea983a424e5b2a0e84eb6ed68fa
Amelia Baldwin <amelia01000010@gmail.com>
2022-08-23 16:37:19 +0100

    code deleted

---
+++ script.js
@@ -0,0 +1,17 @@
+import { pokemonArray } from "./data/pokemon.js";
+
+const getCard = (poke) => {
+  return `<div class="card">
+    <img class="card_image" src="${poke.sprite}" alt=""></img>
+    <h1 class="card_title">${poke.name}</h1>
+    <div class="card_content">
+      <p card_text>${poke.name} is a ${poke.types.join("&")} type Pokemon</p>
+    </div>
+  </div>`;
+};
+
+const container = document.querySelector(".card-container");
+pokemonArray.forEach((poke) => {container.innerHTML += getCard(poke);});
+
+const typeFinder = document.querySelector(".typeFinder");
+typeFinder.addEventListener("change",)
